7fb358d1385388f439f4b8160eebf941a4483c4c
AlphaDown <stoda2005@gmail.com>
2020-12-10 09:56:46 +0000

    Added Fancy text, Difficulty Options, Background, and piece dropping

--- script.js
+++ script.js
@@ -2,20 +2,99 @@ var imgSrc = "https://upload.wikimedia.org/wikipedia/commons/7/75/Celeste_box_ar
 
 var imgWidth = 25;
 var imgHeight = 40;
-var rows = 5;
-var columns = 8;
+var rows = 8;
+var columns = 5;
 
-function randomize(param){
-  //build a function which randomizes the pieces of an array
+var options;
+var piecesCompleted = 0;
+
+function randomize(puzzle){
+   for(let i = puzzle.length-1;i>0;i--){
+   var randomIndex = Math.floor(Math.random() * i);
+   var valueAtRandomIndex = puzzle[randomIndex];
+
+    puzzle[randomIndex] = puzzle[i];
+    puzzle[i] = valueAtRandomIndex;
+ }
 }
 
 $(function(){
   $("#pieces>.imgContainer").draggable()
+
+    /* Firstly, use .change to detect any updates with .difficuly, then make a 
+  variable and have the beginning start with this, refrencing the .difficulty. 
+  After that get the children of it which would be options, next specify only the 
+  selected option, and lastly use the JQuery function ".val()" to find the value of the 
+  selected option. */
+  $( ".difficulty" ).selectmenu({
+    change: function(event, ui) 
+    {
+      options = $(this).children("option:selected").val();
+      console.log(options)
+      if(options == "Custom")
+        {
+          $("#rowCol").show()
+        }
+      else
+        {
+          $("#rowCol").hide()
+        }
+    }
+  });
+
+  $( "#rows" ).spinner({
+    min: 1
+  });
+  
+  $( "#columns" ).spinner({
+    min: 1
+  });
+
 })
 
 function init(){
+  if(options == "Easy")
+        {
+          rows = 6;
+          columns = 4;
+        }
+      else if(options == "Normal")
+        {
+          console.log("hard")
+          rows = 8;
+          columns = 5;   
+        }
+      else if(options == "Hard")
+        {
+          rows = 10;
+          columns = 7;    
+        }
+      else if(options == "Novice")
+        {
+          var rows = 16;
+          var columns = 10;    
+        }
+      else if(options == "Expert")
+        {
+          var rows = 20;
+          var columns = 14; 
+        }
+      else if(options == "Custom")
+        {
+          var rows = $( "#rows" ).spinner( "value" );
+          var columns = $( "#columns" ).spinner( "value" );
+        }
+      if(rows == null)
+      {
+        rows = 1;
+        columns = 1;
+      }
   $("h1").html("Solve the puzzle!")
   $("#final").hide()
+
+  $("h1").css({
+    "font-size": 60 + "px"
+  });
 
   var puzzle = []
 
@@ -30,6 +109,7 @@ function init(){
     " imgContainer'></div>");   
     }
   }
+  randomize(puzzle);
 
   puzzle.forEach(function(piece){
     $("#pieces").append(piece)
@@ -41,8 +121,53 @@ function init(){
   });
   
   $("#pieces>.imgContainer").draggable()
+
+ $("#drop-zone > .imgContainer").droppable({
+   drop: function(event,ui){
+
+    var destNum = $(this).attr("class").split(" ")[0]
+    //console.log(destNum)
+
+    var pieceNum = ui.draggable.attr("class").split(" ")[0]
+    //console.log(pieceNum)
+
+    if(pieceNum == destNum) 
+    {
+        //console.log(ui.draggable.find("img"))  
+
+        $(this).append(ui.draggable.find("img"))
+        ui.draggable.addClass("invisible")
+
+        piecesCompleted += 1;
+        console.log("completed " + piecesCompleted);
+        console.log("total pieces " + puzzle.length);
+        
+        if(piecesCompleted == puzzle.length){
+          $("h1").html("Puzzle Completed! Click to Replay!")
+          $("h1").css({
+            "font-size": 45 + "px"
+          });
+          $(".picture").empty()
+          $("#final").show()
+          piecesCompleted = 0;
+        }
+    }
+    else{
+     ui.draggable.css({
+       "border":".1rem solid red"
+       });
+    }
+   }
+ });
+  //var attributes = $("#pieces").attr("class").split(" ")
+  //console.log(attributes)
 }
+
+
 
 $(document).ready(function(){
   $("#final").click(init)
+  $( ".difficulty" ).selectmenu()
+  $("#rowCol").hide()
+
 });
